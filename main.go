aab5846083366188916a92806e0158d8d4d40e3c
Livio Sgier <livio.sgier@gmail.com>
2017-08-10 18:45:32 +0200

    Working on CLI application that sends txs out in the network, parameters given as arguments. AccTx, fundsTx completed, configTx to follow.

--- main.go
+++ main.go
@@ -1,237 +1,279 @@
 package main
 
 import (
+	"os"
+	"fmt"
+	"errors"
+	"strconv"
+	"bufio"
+	"math/big"
 	"crypto/ecdsa"
+	"crypto/elliptic"
 	"github.com/lisgie/bazo_miner/protocol"
 	"github.com/lisgie/bazo_miner/p2p"
-	"fmt"
 	"net"
-	"time"
-	"math/rand"
-	"math/big"
-	"crypto/elliptic"
+	"strings"
 	"bytes"
 	"encoding/binary"
 	"golang.org/x/crypto/sha3"
 )
 
-const(
-	pubA1 = "c2be9abbeaec39a066c2a09cee23bb9ab2a0b88f2880b1e785b4d317adf0dc7c"
-	pubA2 = "8ce020fde838d9c443f6c93345dafe7fd74f091c4d2f30b37e2453679a257ed5"
-	privA = "ba127fa8f802b008b9cdb58f4e44809d48f1b000cff750dda9cd6b312395c1c5"
-	pubB1 = "5d7eefd58e3d2f309471928ab4bbd104e52973372c159fa652b8ca6b57ff68b8"
-	pubB2 = "ab301a6a77b201c416ddc13a2d33fdf200a5302f6f687e0ea09085debaf8a1d9"
-	privB = "7a0a9babcc97ea7991ed67ed7f800f70c5e04e99718960ad8efab2ca052f00c7"
-)
-
-const (
-	VERSION_ID = 1
-)
-
-var accA, accB protocol.Account
-var PrivKeyA ecdsa.PrivateKey
-var RootPrivKey ecdsa.PrivateKey
-
 func main() {
-	prepAccs()
 
-	var conn net.Conn
+	var (
+		err error
+		msgType uint8
+		tx protocol.Transaction
+	)
 
-	conn,err := net.Dial("tcp", "127.0.0.1:8000")
+	switch os.Args[1] {
+	case "accTx":
+		tx, err = parseAccTx(os.Args[2:])
+		msgType = p2p.ACCTX_BRDCST
+	case "fundsTx":
+		tx, err = parseFundsTx(os.Args[2:])
+		msgType = p2p.FUNDSTX_BRDCST
+	case "configTx":
+		err = parseConfigTx(os.Args[2:])
+		msgType = p2p.CONFIGTX_BRDCST
+	default:
+		fmt.Printf("Usage: bazo_client [accTx|fundsTx|configTx] ...\n")
+		return
+	}
 
 	if err != nil {
 		fmt.Printf("%v\n", err)
 		return
 	}
 
-	packet := p2p.BuildPacket(p2p.MINER_PING,nil)
-	conn.Write(packet)
+	//Transaction creation successful
+	packet := p2p.BuildPacket(msgType, tx.Encode())
 
-	buf := make([]byte, 4096)
-
-	_,err = conn.Read(buf)
+	//Open a connection
+	conn, err := net.Dial("tcp", p2p.BOOTSTRAP_SERVER)
 	if err != nil {
 		fmt.Printf("%v\n", err)
-		conn.Close()
 		return
 	}
+	n,err := conn.Write(packet)
 
-	//sending a fundstx broadcast
-	var txCnt uint32
-	rand := rand.New(rand.NewSource(time.Now().Unix()))
-	tx, _ := protocol.ConstrFundsTx(0x02,rand.Uint64()%100+1, rand.Uint64()%50+1, txCnt, accA.Hash(),accB.Hash(), &PrivKeyA)
+	if n != len(packet) || err != nil {
+		fmt.Printf("Transmission failed\n")
+	}
 
-
-	time.Sleep(3*time.Second)
-	fmt.Printf("%v\n", tx)
-	packet = p2p.BuildPacket(p2p.FUNDSTX_BRDCST,tx.Encode())
-	conn.Write(packet)
-
+	fmt.Printf("Successfully sent the following tansaction:\n%v\n", tx)
 
 	conn.Close()
-
-
-
-
-	/*sendAccReq()
-	sendFundsTx()
-	sendAccTx()
-	sendAccTx()
-	sendAccTx()
-	sendAccTx()
-	time.Sleep(5*time.Second)
-	sendAccReq()*/
 }
 
-func prepAccs() {
+func parseAccTx(args []string) (protocol.Transaction, error) {
 
-	puba1,_ := new(big.Int).SetString(pubA1,16)
-	puba2,_ := new(big.Int).SetString(pubA2,16)
-	priva,_ := new(big.Int).SetString(privA,16)
-	PubKeyA := ecdsa.PublicKey{
+	if len(args) != 4 {
+		return nil, errors.New("Wrong number of arguments.\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>")
+	}
+
+	header, err := strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	fee, err := strconv.Atoi(args[1])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	//Parse the file
+	file,err := os.Open(args[2])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	//Buffered reader because we want to extract the strings themselves, converting is done at a later step
+	reader := bufio.NewReader(file)
+
+	//Public Key
+	pub1, err := reader.ReadString('\n')
+	pub2, err2 := reader.ReadString('\n')
+	//Private Key
+	priv, err3 := reader.ReadString('\n')
+	if err != nil || err2 != nil || err3 != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	//Reconstruct the key
+	//ReadString appends the delim itself, have to remove that, otherwise not parsable into []byte
+	pub1Int, b := new(big.Int).SetString(strings.Split(pub1,"\n")[0], 16)
+	pub2Int, b2 := new(big.Int).SetString(strings.Split(pub2,"\n")[0], 16)
+	privInt, b3 := new(big.Int).SetString(strings.Split(priv,"\n")[0], 16)
+	if !b || !b2 || !b3 {
+		return nil, errors.New("Failed to convert the key strings to big.Int.\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>")
+	}
+	pubKey := ecdsa.PublicKey{
 		elliptic.P256(),
-		puba1,
-		puba2,
+		pub1Int,
+		pub2Int,
 	}
-	PrivKeyA = ecdsa.PrivateKey{
-		PubKeyA,
-		priva,
+	privKey := ecdsa.PrivateKey{
+		pubKey,
+		privInt,
 	}
 
-	pubb1,_ := new(big.Int).SetString(pubB1,16)
-	pubb2,_ := new(big.Int).SetString(pubB2,16)
-	privb,_ := new(big.Int).SetString(privB,16)
-	PubKeyB := ecdsa.PublicKey{
+	tx,newKey,err := protocol.ConstrAccTx(byte(header), uint64(fee), &privKey)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	if tx == nil {
+		return nil, errors.New("Transaction encoding failed.\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>")
+	}
+
+	//Write the public key to the given textfile
+	if _, err = os.Stat(args[3]); !os.IsNotExist(err) {
+		fmt.Print("Output file exists.\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>")
+	}
+
+	file, err = os.Create(args[3])
+	if err != nil {
+		fmt.Printf(fmt.Sprintf("%v\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>", err))
+	}
+
+	_,err = file.WriteString(string(newKey.X.Text(16))+"\n")
+	_,err2 = file.WriteString(string(newKey.Y.Text(16))+"\n")
+	_,err3 = file.WriteString(string(newKey.D.Text(16)))
+
+	if err != nil || err2 != nil || err3 != nil {
+		fmt.Printf("Failed to write key to file\nUsage: bazo_client accTx <header> <fee> <privKey> <keyOutput>")
+	}
+
+	return tx, nil
+}
+
+func parseFundsTx(args []string) (protocol.Transaction, error) {
+
+	var (
+		fromPubKey, toPubKey [64]byte
+	)
+
+	if len(args) != 7 {
+		return nil, errors.New("Wrong number of arguments.\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>")
+	}
+
+	header, err := strconv.Atoi(args[0])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	amount, err := strconv.Atoi(args[1])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	fee, err := strconv.Atoi(args[2])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	txCnt, err := strconv.Atoi(args[3])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	hashFromFile, err := os.Open(args[4])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	reader := bufio.NewReader(hashFromFile)
+	//We only need the public key
+	pub1, err := reader.ReadString('\n')
+	pub2, err2 := reader.ReadString('\n')
+	if err != nil || err2 != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	pub1Int,_ := new(big.Int).SetString(strings.Split(pub1,"\n")[0], 16)
+	pub2Int,_ := new(big.Int).SetString(strings.Split(pub2,"\n")[0], 16)
+	copy(fromPubKey[0:32], pub1Int.Bytes())
+	copy(fromPubKey[32:64], pub2Int.Bytes())
+
+	hashToFile, err := os.Open(args[5])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	reader.Reset(hashToFile)
+	//We only need the public key
+	pub1, err = reader.ReadString('\n')
+	pub2, err2 = reader.ReadString('\n')
+	if err != nil || err2 != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	pub1Int,_ = new(big.Int).SetString(strings.Split(pub1,"\n")[0], 16)
+	pub2Int,_ = new(big.Int).SetString(strings.Split(pub2,"\n")[0], 16)
+	copy(toPubKey[0:32], pub1Int.Bytes())
+	copy(toPubKey[32:64], pub2Int.Bytes())
+
+	keyFile, err := os.Open(args[6])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+	reader.Reset(keyFile)
+
+	//Public Key
+	pub1, err = reader.ReadString('\n')
+	pub2, err2 = reader.ReadString('\n')
+	//Private Key
+	priv, err3 := reader.ReadString('\n')
+	if err != nil || err2 != nil || err3 != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	pub1Int, b := new(big.Int).SetString(strings.Split(pub1,"\n")[0], 16)
+	pub2Int, b2 := new(big.Int).SetString(strings.Split(pub2,"\n")[0], 16)
+	privInt, b3 := new(big.Int).SetString(strings.Split(priv,"\n")[0], 16)
+	if !b || !b2 || !b3 {
+		return nil, errors.New("Failed to convert the key strings to big.Int.\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>")
+	}
+	pubKey := ecdsa.PublicKey{
 		elliptic.P256(),
-		pubb1,
-		pubb2,
+		pub1Int,
+		pub2Int,
 	}
-	PrivKeyB := ecdsa.PrivateKey{
-		PubKeyB,
-		privb,
-	}
-
-	accA = protocol.Account{Balance: 15000}
-	copy(accA.Address[0:32], PrivKeyA.PublicKey.X.Bytes())
-	copy(accA.Address[32:64], PrivKeyA.PublicKey.Y.Bytes())
-	//accA.Hash = sha3.Sum256(accA.Address[:])
-
-
-	//This one is just for testing purposes
-	accB = protocol.Account{Balance: 702}
-	copy(accB.Address[0:32], PrivKeyB.PublicKey.X.Bytes())
-	copy(accB.Address[32:64], PrivKeyB.PublicKey.Y.Bytes())
-	//accB.Hash = sha3.Sum256(accB.Address[:])
-
-
-	var pubKey [64]byte
-
-	pub1,_ := new(big.Int).SetString(protocol.RootPub1,16)
-	pub2,_ := new(big.Int).SetString(protocol.RootPub2,16)
-	priv,_ := new(big.Int).SetString(protocol.RootPriv,16)
-	PubKeyA = ecdsa.PublicKey{
-		elliptic.P256(),
-		pub1,
-		pub2,
-	}
-	RootPrivKey = ecdsa.PrivateKey{
-		PubKeyA,
-		priv,
+	privKey := ecdsa.PrivateKey{
+		pubKey,
+		privInt,
 	}
 
-	copy(pubKey[32-len(pub1.Bytes()):32],pub1.Bytes())
-	copy(pubKey[64-len(pub2.Bytes()):],pub2.Bytes())
+	tx, err := protocol.ConstrFundsTx(
+		byte(header),
+		uint64(amount),
+		uint64(fee),
+		uint32(txCnt),
+		serializeHashContent(fromPubKey[:]),
+		serializeHashContent(toPubKey[:]),
+		&privKey,
+	)
 
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("%v\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>", err))
+	}
+
+	if tx == nil {
+		return nil, errors.New("Transaction encoding failed.\nUsage: bazo_client fundsTx <header> <amount> <fee> <txCnt> <fromHash> <toHash> <privKey>")
+	}
+
+	return tx, nil
+}
+
+func parseConfigTx(args []string) error {
+
+	return nil
+}
+
+func serializeHashContent(data interface{}) (hash [32]byte) {
 
 	var buf bytes.Buffer
-
-	binary.Write(&buf,binary.LittleEndian, pubKey[:])
-
-	rootHash :=  sha3.Sum256(buf.Bytes())
-
-	var shortRootHash [8]byte
-	copy(shortRootHash[:], rootHash[0:8])
-	//rootAcc := bc.Account{Hash:rootHash, Address:pubKey}
+	binary.Write(&buf, binary.BigEndian, data)
+	return sha3.Sum256(buf.Bytes())
 }
-
-/*func sendAccReq() {
-	var conn net.Conn
-	conn, _ = net.Dial("tcp", "127.0.0.1:8081")
-	header := p2p.ConstructHeader(len(accA.Hash), protocol.ACC_REQ)
-	toSend := make([]byte, len(header)+len(accA.Hash))
-	copy(toSend[:p2p.HEADER_LEN],header[:])
-	copy(toSend[p2p.HEADER_LEN:],accA.Hash[:])
-
-	conn.Write(toSend)
-
-	reader := bufio.NewReader(conn)
-	recvHeader := protocol.ExtractHeader(reader)
-
-	recvBuf := make([]byte, recvHeader.Len)
-	for i := 0; i < int(recvHeader.Len); i++ {
-		in,_ := reader.ReadByte()
-		recvBuf[i] = in
-	}
-
-	acc := protocol.DecodeAcc(recvBuf)
-	fmt.Printf("%v\n", acc)
-	conn.Close()
-}
-
-func sendAccTx() {
-	var conn net.Conn
-	rand := rand.New(rand.NewSource(time.Now().Unix()))
-	conn, _ = net.Dial("tcp", "127.0.0.1:8081")
-	tx,_ := protocol.ConstrAccTx(rand.Uint64()%100+1,&RootPrivKey)
-	accData := protocol.EncodeAccTx(tx)
-	header := protocol.ConstructHeader(len(accData), protocol.ACCTX)
-
-	toSend := make([]byte, len(accData)+len(header))
-	copy(toSend[0:protocol.HEADER_LEN],header[:])
-	copy(toSend[protocol.HEADER_LEN:],accData[:])
-	conn.Write(toSend)
-
-	conn.Close()
-}
-
-func sendFundsTx() {
-	var conn net.Conn
-	var txCnt uint32
-	rand := rand.New(rand.NewSource(time.Now().Unix()))
-	conn, _ = net.Dial("tcp", "127.0.0.1:8081")
-	tx, _ := protocol.ConstrFundsTx(0x02,rand.Uint64()%100+1, rand.Uint64()%50+1, txCnt, accA.Hash,accB.Hash, &PrivKeyA)
-	fundsData := protocol.EncodeFundsTx(tx)
-	header := protocol.ConstructHeader(len(fundsData), protocol.FUNDSTX)
-
-	toSend := make([]byte, len(fundsData)+len(header))
-	copy(toSend[0:protocol.HEADER_LEN],header[:])
-	copy(toSend[protocol.HEADER_LEN:],fundsData[:])
-
-	conn.Write(toSend)
-	conn.Close()
-}
-
-func sendTimeReq() {
-	var conn net.Conn
-
-	conn, _ = net.Dial("tcp", "127.0.0.1:8081")
-
-	header := protocol.ConstructHeader(0, protocol.TIME_REQ)
-	toSend := make([]byte, protocol.HEADER_LEN)
-	copy(toSend[:], header[:])
-	conn.Write(toSend)
-
-	reader := bufio.NewReader(conn)
-	recvHeader := protocol.ExtractHeader(reader)
-
-	recvBuf := make([]byte, recvHeader.Len)
-	for i := 0; i < int(recvHeader.Len); i++ {
-		in,_ := reader.ReadByte()
-		recvBuf[i] = in
-	}
-
-	conn.Close()
-}
-
-
-*/
